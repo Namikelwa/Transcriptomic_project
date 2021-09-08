b04d8e7a4510bc89ca44d9ddcc20add2a816ac32
Dorcus Namikelwa <85280529+Namikelwa@users.noreply.github.com>
2021-09-08 11:36:43 +0300

    Update raw-data.sh

--- scripts/raw-data.sh
+++ scripts/raw-data.sh
@@ -1,1 +1,6 @@
+esearch -db sra -query PRJNA560504 | efetch --format runinfo | cut -d "," -f 1 > SraAccList.txt
+
+# esearch command uses the ESearch utility to search on the NCBI database for query and finds the unique identifiers for all query that match the search query.
+#-db database
+#efetch downloads selected records in a style designated by -format
 
