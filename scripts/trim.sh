db9f30b04d0f2fb1d32d310935efac30b895089a
Dorcus Namikelwa <85280529+Namikelwa@users.noreply.github.com>
2021-09-08 11:40:30 +0300

    Update trim.sh

--- scripts/trim.sh
+++ scripts/trim.sh
@@ -1,1 +1,13 @@
+#basename is a command that strips trailing suffix in a file name
+for i in *_1.fastq.gz;
+do
+    name=$(basename ${i} _1.fastq.gz)
+    trimmomatic PE ${i} ${name}_2.fastq.gz\
+                   ${name}_1.trim.fastq.gz ${name}_1un.trim.fastq.gz \
+                    ${name}_2.trim.fastq.gz ${name}_2un.trim.fastq.gz \
+                    HEADCROP:11
+done
+PE - paired end
+HEADCROP -removes the first 11 bases of the reads
+
 
