e90c44cae13814ac75dc745ec83ddbb17238c6aa
Dorcus Namikelwa <85280529+Namikelwa@users.noreply.github.com>
2021-09-08 11:38:55 +0300

    Update raw-data.sh

--- scripts/raw-data.sh
+++ scripts/raw-data.sh
@@ -1,6 +1,18 @@
+# Getting accession numbers from NCBI
+
 esearch -db sra -query PRJNA560504 | efetch --format runinfo | cut -d "," -f 1 > SraAccList.txt
 
 # esearch command uses the ESearch utility to search on the NCBI database for query and finds the unique identifiers for all query that match the search query.
 #-db database
 #efetch downloads selected records in a style designated by -format
+
+# Downloading the data
+mkdir raw-data
+cd raw-data
+for i in $(cat …/SraAccList.txt);    #SraAcclis.txt contains a list of the accesion numbers
+do
+    echo $i
+    fastq-dump --gzip --split-files $i  #fastq-dump gets data in fastq format
+done
+
 
